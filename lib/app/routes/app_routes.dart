e10c0a705f19dc4c4e6ec913f3f1b15cc3749462
Hai Saiful Rizal <syaifulrizal230206@gmail.com>
2026-05-11 17:17:11 +0700

    Delete lib directory

--- lib/app/routes/app_routes.dart
+++
@@ -1,6 +0,0 @@
-abstract class AppRoutes {
-  static const String loading = '/';
-  static const String login = '/login';
-  static const String register = '/register';
-  static const String shell = '/shell';
-}
